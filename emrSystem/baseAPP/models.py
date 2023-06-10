86ab20a667bba8fdbc5687fcd41cc954fc8416c9
Benjamin Gambill <bencgambill@gmail.com>
2023-06-10 17:33:14 -0400

    retry

--- emrSystem/baseAPP/models.py
+++ emrSystem/baseAPP/models.py
@@ -5,9 +5,9 @@
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
+import re
 from django.db import models
 from django.core.exceptions import ValidationError
-
 
 class AuthGroup(models.Model):
     name = models.CharField(unique=True, max_length=150)
