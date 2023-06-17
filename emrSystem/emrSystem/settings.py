b29476200fb91e739743b4a8aa195bd279961030
Wesley <89660042+WesTheHuman2000@users.noreply.github.com>
2023-06-17 17:13:17 -0400

    fix

--- emrSystem/emrSystem/settings.py
+++ emrSystem/emrSystem/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'emrsystem',
         'USER': 'root',
-        'PASSWORD': 'Beer4d0g***',
+        'PASSWORD': 'admin',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
